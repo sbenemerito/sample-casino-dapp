595ebbc9993acabd0ea56d3dcff1e8ccef370472
Sam Benemerito <benemeritosam@gmail.com>
2018-10-20 20:49:10 +0900

    interact with Casino contract on Remix

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,272 @@ import ReactDOM from 'react-dom'
 import Web3 from 'web3'
 import './../css/index.css'
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lastWinner: 0,
-      timer: 0
+      numberOfBets: 0,
+      minimumBet: 0,
+      totalBet: 0,
+      maxAmountOfBets: 0
     };
+
+    if (typeof web3 != 'undefined') {
+      console.log("Using web3 detected from external source like MetaMask");
+      this.web3 = new Web3(web3.currentProvider);
+    } else {
+      this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+    }
+
+    const MyContract = web3.eth.contract([
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "numberOfBets",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "_player",
+            "type": "address"
+          }
+        ],
+        "name": "checkPlayerExists",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [],
+        "name": "kill",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "playerInfo",
+        "outputs": [
+          {
+            "name": "amountBet",
+            "type": "uint256"
+          },
+          {
+            "name": "numberSelected",
+            "type": "uint8"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "maximumNumberOfBets",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "minimumBet",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_numberSelected",
+            "type": "uint8"
+          }
+        ],
+        "name": "bet",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "players",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "totalBet",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "name": "_minimumBet",
+            "type": "uint256"
+          },
+          {
+            "name": "_maximumPlayers",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+      }
+    ]);
+    this.state.ContractInstance = MyContract.at("0xda1442d2774d43d2316b3329f8238724891b009b");
   }
 
-  betNumber(number) {
-    console.log(number);
+  componentDidMount() {
+    this.updateState();
+    this.setupListeners();
+
+    setInterval(this.updateState.bind(this), 10e3);
+  }
+
+  updateState() {
+    this.state.ContractInstance.minimumBet((err, result) => {
+      if (result != null) {
+        this.setState({
+          minimumBet: parseFloat(web3.fromWei(result, 'ether'))
+        });
+      }
+    });
+    this.state.ContractInstance.totalBet((err, result) => {
+      if (result != null) {
+        this.setState({
+          totalBet: parseFloat(web3.fromWei(result, 'ether'))
+        });
+      }
+    });
+    this.state.ContractInstance.numberOfBets((err, result) => {
+      if (result != null) {
+        this.setState({
+          numberOfBets: parseInt(result)
+        });
+      }
+    });
+    this.state.ContractInstance.maximumNumberOfBets((err, result) => {
+      if (result != null) {
+        this.setState({
+          maximumNumberOfBets: parseInt(result)
+        });
+      }
+    });
+  }
+
+  setupListeners() {
+    let liNodes = this.refs.numbers.querySelectorAll('li');
+    liNodes.forEach(number => {
+      number.addEventListener('click', event => {
+        event.target.className = 'number-selected';
+        this.betNumber(parseInt(event.target.innerHTML), done => {
+          for(let i = 0; i < liNodes.length; i++){
+            liNodes[i].className = '';
+          }
+        })
+      });
+    });
+  }
+
+  betNumber(number, callBack) {
+    let bet = this.refs['ether-bet'].value;
+    if (!bet) bet = 0.1;
+
+    if (parseFloat(bet) < this.state.minimumBet) {
+      alert('You must bet more than the minimum');
+      callBack();
+    } else {
+      this.state.ContractInstance.bet(number, {
+        gas: 300000,
+        from: web3.eth.accounts[0],
+        value: web3.toWei(bet, 'ether')
+      }, (err, result) => {
+        callBack()
+      })
+    }
   }
 
   render() {
@@ -21,28 +276,46 @@ class App extends React.Component {
       <div className="main-container">
         <h1>Bet for your best number and win huge amounts of Ether</h1>
         <div className="block">
-          <h4>Timer:</h4> &nbsp;
-          <span ref="timer"> {this.state.timer}</span>
+          <b>Number of bets:</b> &nbsp;
+          <span>{this.state.numberOfBets}</span>
         </div>
         <div className="block">
-          <h4>Last winner:</h4> &nbsp;
-          <span ref="last-winner">{this.state.lastWinner}</span>
+          <b>Last number winner:</b> &nbsp;
+          <span>{this.state.lastWinner}</span>
+        </div>
+        <div className="block">
+          <b>Total ether bet:</b> &nbsp;
+          <span>{this.state.totalBet} ether</span>
+        </div>
+        <div className="block">
+          <b>Minimum bet:</b> &nbsp;
+          <span>{this.state.minimumBet} ether</span>
+        </div>
+        <div className="block">
+          <b>Maximum number of bets:</b> &nbsp;
+          <span>{this.state.maximumNumberOfBets}</span>
         </div>
         <hr/>
-        <h2>Bet for the next number</h2>
-          <ul>
-            <li onClick={() => {this.betNumber(1)}}>1</li>
-            <li onClick={() => {this.betNumber(2)}}>2</li>
-            <li onClick={() => {this.betNumber(3)}}>3</li>
-            <li onClick={() => {this.betNumber(4)}}>4</li>
-            <li onClick={() => {this.betNumber(5)}}>5</li>
-            <li onClick={() => {this.betNumber(6)}}>6</li>
-            <li onClick={() => {this.betNumber(7)}}>7</li>
-            <li onClick={() => {this.betNumber(8)}}>8</li>
-            <li onClick={() => {this.betNumber(9)}}>9</li>
-            <li onClick={() => {this.betNumber(10)}}>10</li>
-          </ul>
+        <h2>Vote for the next number</h2>
+        <label>
+          <b>How much Ether do you want to bet? <input className="bet-input" ref="ether-bet" type="number" placeholder={this.state.minimumBet}/></b> ether
+          <br/>
+        </label>
+        <ul ref="numbers">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+        </ul>
       </div>
     )
   }
 }
+
+ReactDOM.render(<App/>, document.getElementById('root'));
