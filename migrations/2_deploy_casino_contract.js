6b331e3bb4ae5f813e0938237d4832717ba9a337
Sam Benemerito <benemeritosam@gmail.com>
2018-10-20 16:12:58 +0900

    fix truffle compilation

--- migrations/2_deploy_casino_contract.js
+++ migrations/2_deploy_casino_contract.js
@@ -1,5 +1,5 @@
 var Casino = artifacts.require("./Casino.sol");
 
-module.explorts = function(deployer) {
+module.exports = function(deployer) {
     deployer.deploy(web3.toWei(0.1, 'ether'), 5, {gas: 3000000});
 };
