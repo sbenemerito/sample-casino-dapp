cb67a6ff0377c1491e868a74121ad38ebd693a28
Sam Benemerito <benemeritosam@gmail.com>
2018-10-20 20:48:48 +0900

    Fix webpack + React build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
+  entry: path.join(__dirname, 'src/js/index.js'), // Our frontend will be inside the src folder,
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist/'),
     filename: 'build.js' // The final file will be created in dist/build.js
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/'),
+    hot: true
   },
   module: {
     rules: [{
-      test: /\.css$/, // To load the css in react
-      use: ['style-loader', 'css-loader'],
-      include: /src/
+        test: /\.css$/, // To load the css in react
+        loader: ['style-loader', 'css-loader'],
+        include: /src/
     }, {
-      test: /\.jsx?$/, // To load the js and jsx files
-      loader: 'babel-loader',
-      exclude: /node_modules/
+        test: /\.jsx?$/, // To load the js and jsx files
+        loader: 'babel-loader',
+        exclude: /node_modules/
     }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  watch: true,
+  watchOptions: {
+    poll: 500 // Check for changes every second
   }
 }
